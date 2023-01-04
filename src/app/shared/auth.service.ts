4060dcbf8cec96f6bebf7401c566a3c1450fa2aa
Dell <ibrahimkrimi2@gmail.com>
2023-01-04 22:48:20 +0100

    final commit ya rabi

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -10,7 +10,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class AuthService {
   loggedIn=false;
   admin=false;
-  url="https://angular-back.onrender.com/api/user";
+  url="https://angular-back.onrender.com/api/users";
   user:User;
   private HttpOptions = {
     headers: new HttpHeaders({
